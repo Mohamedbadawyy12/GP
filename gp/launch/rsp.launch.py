2fa11f9119eb9644067df194a64be5ecedb6f695
Mohamed badawy <mmohamedbadawyyy968@gmail.com>
2024-06-23 19:50:07 -0400

    second

--- gp/launch/rsp.launch.py
+++ gp/launch/rsp.launch.py
@@ -3,27 +3,27 @@ from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import Command
 from launch_ros.actions import Node
 from launch import LaunchDescription
-
+import xacro
 
 # this is the function launch system will look for
 def generate_launch_description():
 
     ####### DATA INPUT ########
-    urdf_file_name = 'robot.urdf'
+    urdf_file_name = 'robot.urdf.xacro'
     package_name = 'gp'
     ####### DATA INPUT END ########
 
     print("Fetching URDF ==>")
     # path to our package/urdf/urdf_file
     robot_desc_path = os.path.join(get_package_share_directory(package_name), "urdf", urdf_file_name)
-
+    xacro_file =xacro.process_file(robot_desc_path)
     # Robot State Publisher
     urdf_content = open(robot_desc_path).read()
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher_node',
-        parameters=[{'use_sim_time': True, 'robot_description': urdf_content}],
+        parameters=[{'use_sim_time': True, 'robot_description': xacro_file.toxml()}],
         output='screen'
     )
 
